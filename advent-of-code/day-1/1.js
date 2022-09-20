dee83ca236d6510bd2a1b086c5a8e01ea3b74bf9
octobercold <theminddepth@gmail.com>
2022-09-20 12:04:39 +0100

    solve day 1 part two

--- advent-of-code/day-1/1.js
+++ advent-of-code/day-1/1.js
@@ -4,7 +4,6 @@ var string = fs.readFileSync(path + "/input.txt", { encoding: "utf-8" });
 var lines = string.split("\n");
 
 const nums = lines.map((line) => parseInt(line));
-console.log(nums);
 
 const countIncreasingDepth = (nums) => {
     const l = nums.length;
@@ -16,4 +15,19 @@ const countIncreasingDepth = (nums) => {
     }
     return res;
 };
-console.log(countIncreasingDepth(nums));
+
+const countIncreasingWindowDepth = (nums) => {
+    const l = nums.length;
+    let res = 0,
+        curr = 0,
+        next = 0;
+    for (i = 2; i < l; i++) {
+        if (!nums[i + 1]) break;
+        curr = nums[i] + nums[i - 1] + nums[i - 2];
+        next = nums[i + 1] + nums[i] + nums[i - 1];
+        if (next > curr) {
+            res++;
+        }
+    }
+    return res;
+};
