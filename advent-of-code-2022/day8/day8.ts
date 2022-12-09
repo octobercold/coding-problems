dfc62f60eaa96c341b6e1f842c832894942e8bbb
octobercold <theminddepth@gmail.com>
2022-12-09 15:04:54 +0000

    aoc 2022 day 8 part one brush up

--- advent-of-code-2022/day8/day8.ts
+++ advent-of-code-2022/day8/day8.ts
@@ -19,40 +19,40 @@ const checkTop = (starti: number, startj: number): boolean => {
     const treeHeight = trees[starti][startj];
     for (let i = starti - 1; i >= 0; i--) {
         if (treeHeight <= trees[i][startj]) {
-            return true;
+            return false;
         }
     }
-    return false;
+    return true;
 };
 
 const checkRight = (starti: number, startj: number): boolean => {
     const treeHeight = trees[starti][startj];
     for (let j = startj + 1; j < trees[starti].length; j++) {
         if (treeHeight <= trees[starti][j]) {
-            return true;
+            return false;
         }
     }
-    return false;
+    return true;
 };
 
 const checkDown = (starti: number, startj: number): boolean => {
     const treeHeight = trees[starti][startj];
     for (let i = starti + 1; i < trees.length; i++) {
         if (treeHeight <= trees[i][startj]) {
-            return true;
+            return false;
         }
     }
-    return false;
+    return true;
 };
 
 const checkLeft = (starti: number, startj: number): boolean => {
     const treeHeight = trees[starti][startj];
     for (let j = startj - 1; j >= 0; j--) {
         if (treeHeight <= trees[starti][j]) {
-            return true;
+            return false;
         }
     }
-    return false;
+    return true;
 };
 
 let numberOfVisibleTrees = 0;
@@ -73,28 +73,28 @@ for (let i = 0; i < trees.length; i++) {
             numberOfVisibleTrees++;
             continue;
         }
-        if (!checkTop(i, j)) {
+        if (checkTop(i, j)) {
             console.log(
                 `tree height ${trees[i][j]} at [${i},${j}] is visible from top`
             );
             numberOfVisibleTrees++;
             continue;
         }
-        if (!checkRight(i, j)) {
+        if (checkRight(i, j)) {
             console.log(
                 `tree height ${trees[i][j]} at [${i},${j}] is visible from right`
             );
             numberOfVisibleTrees++;
             continue;
         }
-        if (!checkDown(i, j)) {
+        if (checkDown(i, j)) {
             console.log(
                 `tree height ${trees[i][j]} at [${i},${j}] is visible from bottom`
             );
             numberOfVisibleTrees++;
             continue;
         }
-        if (!checkLeft(i, j)) {
+        if (checkLeft(i, j)) {
             console.log(
                 `tree height ${trees[i][j]} at [${i},${j}] is visible from left`
             );
