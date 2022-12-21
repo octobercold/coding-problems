aa71de44f3b5fbe28f9e30a432659fd48a3eb909
octobercold <theminddepth@gmail.com>
2022-12-21 23:19:32 +0000

    aoc 2022 day12 working towards efficient solution

--- advent-of-code-2022/day12/day12.ts
+++ advent-of-code-2022/day12/day12.ts
@@ -1,12 +1,12 @@
 import * as fs from "fs";
 
-const fileReader = () => {
-    //returns list of strings separated by \n
-    const string = fs.readFileSync("./day12/input.txt", { encoding: "utf-8" });
-    return string.split("\n");
-};
+// const fileReader = () => {
+//     //returns list of strings separated by \n
+//     const string = fs.readFileSync("./day12/input.txt", { encoding: "utf-8" });
+//     return string.split("\n");
+// };
 
-const lines = fileReader();
+// const lines = fileReader();
 
 /*
 S = 83
@@ -23,141 +23,129 @@ z = 122
 ]
 */
 
-const map = [];
-const start = { i: -1, j: -1 };
-const end = { i: -1, j: -1 };
+// const map = [];
+// const start = { i: -1, j: -1 };
+// const end = { i: -1, j: -1 };
 
-for (let i = 0; i < lines.length; i++) {
-    const row = [];
-    for (let j = 0; j < lines[i].length; j++) {
-        if (lines[i][j] === "S") {
-            start.i = i;
-            start.j = j;
-            row.push(0);
-        } else if (lines[i][j] === "E") {
-            end.i = i;
-            end.j = j;
-            row.push(25);
-        } else {
-            row.push(lines[i][j].charCodeAt(0) - 97);
-        }
+// for (let i = 0; i < lines.length; i++) {
+//     const row = [];
+//     for (let j = 0; j < lines[i].length; j++) {
+//         if (lines[i][j] === "S") {
+//             start.i = i;
+//             start.j = j;
+//             row.push(0);
+//         } else if (lines[i][j] === "E") {
+//             end.i = i;
+//             end.j = j;
+//             row.push(25);
+//         } else {
+//             row.push(lines[i][j].charCodeAt(0) - 97);
+//         }
+//     }
+//     map.push(row);
+// }
+
+// console.log(`start: ${start.i}, ${start.j}`);
+// console.log(`end: ${end.i}, ${end.j}`);
+
+class Graph {
+    walls: Set<string>;
+    constructor(public width: number, public height: number) {
+        this.walls = new Set();
     }
-    map.push(row);
-}
 
-console.log(`start: ${start.i}, ${start.j}`);
-console.log(`end: ${end.i}, ${end.j}`);
-
-const maze = [
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
-
-interface Coordinate {
-    i: number;
-    j: number;
-}
-
-class Node {
-    g: number;
-    h: number;
-    f: number;
-
-    constructor(public position: Coordinate, public parent?: Node) {
-        this.position = position;
-        this.parent = parent === undefined ? null : parent;
-        this.g = 0;
-        this.h = 0;
-        this.f = 0;
+    inBounds(id: [number, number]): boolean {
+        const [x, y] = id;
+        return x >= 0 && x < this.width && y >= 0 && y < this.height;
     }
-}
 
-const astar = (map: number[][], start: Coordinate, end: Coordinate) => {
-    const startNode = new Node(start);
-    const endNode = new Node(end);
+    passable(id: [number, number]): boolean {
+        return !this.walls.has(`${id[0]},${id[1]}`);
+    }
 
-    const openList = [startNode];
-    const closedList: Node[] = [];
-
-    while (openList.length > 0) {
-        console.log(`open list: ${openList}`);
-        console.log(`closed list: ${closedList}`);
-        let currentNode = openList[0];
-        const currenVal = map[currentNode.position.i][currentNode.position.j];
-        let currentIndex = 0;
-        for (let i = 0; i < openList.length; i++) {
-            if (openList[i].f < currentNode.f) {
-                currentNode = openList[i];
-                currentIndex = i;
-            }
-        }
-        openList.splice(currentIndex, 1);
-        closedList.push(currentNode);
-
-        if (currentNode === endNode) {
-            const path = [];
-            let current = currentNode;
-            while (currentNode !== undefined) {
-                path.push(current.position);
-                current = current.parent;
-            }
-            return path.reverse();
-        }
-
-        const children: Node[] = [];
-
-        const neighboursPositions: Coordinate[] = [
-            { i: 1, j: 0 },
-            { i: 0, j: 1 },
-            { i: -1, j: 0 },
-            { i: 0, j: -1 },
+    neighbours(id: [number, number]): [number, number][] {
+        const [x, y] = id;
+        const neighbours: [number, number][] = [
+            [x + 1, y],
+            [x - 1, y],
+            [x, y - 1],
+            [x, y + 1],
         ];
-        for (const position of neighboursPositions) {
-            const nodePosition: Coordinate = {
-                i: currentNode.position.i + position.i,
-                j: currentNode.position.j + position.j,
-            };
-            if (
-                nodePosition.i > map.length - 1 ||
-                nodePosition.i < 0 ||
-                nodePosition.j > map[0].length - 1 ||
-                nodePosition.j < 0
-            ) {
-                continue;
-            }
-
-            // if (map[nodePosition.i][nodePosition.j] - 1 > currenVal) {
-            //     continue;
-            // }
-            if (map[nodePosition.i][nodePosition.j] != 0) continue;
-            const newNode = new Node(nodePosition, currentNode);
-            children.push(newNode);
-        }
-
-        for (const child of children) {
-            for (const closedChild of closedList) {
-                if (child === closedChild) continue;
-                child.g = currentNode.g + 1;
-                child.h =
-                    (child.position.i - endNode.position.i) ** 2 +
-                    (child.position.j - endNode.position.j) ** 2;
-                child.f = child.g + child.h;
-            }
-            for (const openNode of openList) {
-                if (child === openNode && child.g > openNode.g) continue;
-                openList.push(child);
-            }
-        }
+        if ((x + y) % 2 === 0) neighbours.reverse();
+        const results = neighbours
+            .filter((neighbour) => this.inBounds(neighbour))
+            .filter((neighbour) => this.passable(neighbour));
+        return results;
     }
+}
+
+const from_id_width = (id: number, width: number): [number, number] => {
+    return [id % width, Math.floor(id / width)];
 };
 
-const path = astar(maze, { i: 0, j: 0 }, { i: 7, j: 6 });
-console.log(path);
+const drawTile = (
+    graph: Graph,
+    id: [number, number],
+    parents: Map<string, [number, number]>
+) => {
+    let r = " A ";
+    const [x1, y1] = id;
+    const parent = parents.get(`${id[0]},${id[1]}`);
+    if (parent !== undefined && parent !== null) {
+        const [x2, y2] = parent;
+        if (x2 === x1 + 1) r = " > ";
+        if (x2 == x1 - 1) r = " < ";
+        if (y2 == y1 + 1) r = " v ";
+        if (y2 == y1 - 1) r = " ^ ";
+    }
+    if (graph.walls.has(`${id[0]},${id[1]}`)) r = "###";
+    return r;
+};
+
+const drawGrid = (graph: Graph, parents: Map<string, [number, number]>) => {
+    console.log("___".repeat(graph.width));
+    for (let y = 0; y < graph.height; y++) {
+        let row = "";
+        for (let x = 0; x < graph.width; x++) {
+            row += drawTile(graph, [x, y], parents);
+        }
+        console.log(row);
+        console.log();
+    }
+    console.log("~~~".repeat(graph.width));
+};
+
+const walls = [
+    21, 22, 51, 52, 81, 82, 93, 94, 111, 112, 123, 124, 133, 134, 141, 142, 153,
+    154, 163, 164, 171, 172, 173, 174, 175, 183, 184, 193, 194, 201, 202, 203,
+    204, 205, 213, 214, 223, 224, 243, 244, 253, 254, 273, 274, 283, 284, 303,
+    304, 313, 314, 333, 334, 343, 344, 373, 374, 403, 404, 433, 434,
+];
+
+const BFS = (graph: Graph, start: [number, number]) => {
+    const frontier: [number, number][] = [];
+    frontier.push(start);
+    const cameFrom: Map<string, [number, number] | null> = new Map();
+    cameFrom.set(`${start[0]},${start[1]}`, null);
+
+    while (frontier.length > 0) {
+        const current: [number, number] = frontier.shift();
+        for (const next of graph.neighbours(current)) {
+            if (!cameFrom.has(`${next[0]},${next[1]}`)) {
+                frontier.push(next);
+                cameFrom.set(`${next[0]},${next[1]}`, current);
+            }
+        }
+    }
+    return cameFrom;
+};
+
+const diagramWalls: [number, number][] = walls.map((id) =>
+    from_id_width(id, 30)
+);
+const g = new Graph(30, 15);
+diagramWalls.forEach((id) => g.walls.add(`${id[0]},${id[1]}`));
+
+const start: [number, number] = [8, 7];
+const parents = BFS(g, start);
+drawGrid(g, parents);
