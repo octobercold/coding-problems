968efddbbbf83d8750238775be94569052314615
octobercold <theminddepth@gmail.com>
2023-02-03 16:10:38 +0000

    aoc 2022 day 22 test case works replaced OOP solution with just map

--- advent-of-code-2022/src/day22.ts
+++ advent-of-code-2022/src/day22.ts
@@ -1,83 +1,127 @@
+import { walkUpBindingElementsAndPatterns } from "typescript";
 import { fileReader } from "./utils/fileReader";
 const lines = fileReader(22);
-let maxL = -Infinity;
-lines.forEach(line => (maxL = Math.max(maxL, line.length)));
+console.log("lines: ", lines);
+const mapStrings = lines.slice(0, -2);
+console.log("mapStrings: ", mapStrings);
 
-class Node {
-    key: string;
-    x: number;
-    y: number;
-    walkable: boolean;
-    constructor(x, y, walkable) {
-        this.key = `${x},${y}`;
-        this.x = x;
-        this.y = y;
-        this.walkable = walkable;
-    }
-
-    getNeighboursKeys() {
-        return [
-            [this.x + 1, this.y], // 0 - right;
-            [this.x, this.y + 1], // 1 - down
-            [this.x - 1, this.y], // 2 - left
-            [this.x, this.y - 1], // 3 - up
-        ];
-    }
+const map: Map<string, string[]> = new Map();
+function getNeighbours(x: number, y: number): number[][] {
+    return [
+        [x + 1, y],
+        [x, y + 1],
+        [x - 1, y],
+        [x, y - 1],
+    ];
 }
 
-const map: Map<string, Node> = new Map();
-const emptySpace: Set<string> = new Set();
+function wrapping(
+    facing: number,
+    nx: number,
+    ny: number,
+    key: string,
+    neighbours: number[][]
+) {
+    let wrappingKey: string;
+    if (facing === 0 || facing === 2) {
+        neighbours[facing] = [-1, ny];
+        wrappingKey = `-1,${ny}`;
+    } else {
+        neighbours[facing] = [nx, -1];
+        wrappingKey = `${nx},-1`;
+    }
 
-const border = [-Infinity, -Infinity, +Infinity, +Infinity];
-let startX = +Infinity;
+    const swap = { 0: 2, 1: 3, 2: 0, 3: 1 };
 
-for (let y = 0; y < lines.length - 2; y++) {
-    const items = lines[y].padEnd(maxL, " ").split("");
-    console.log("items: ", items);
-    for (let x = 0; x < items.length; x++) {
-        if (items[x] === " ") {
-            emptySpace.add(`${x + 1},${y + 1}`);
+    console.log("before: ", wrappingKey, " : ", map.get(wrappingKey));
+
+    if (map.has(wrappingKey)) {
+        const arr = map.get(wrappingKey);
+        arr[swap[facing]] = key;
+        map.set(wrappingKey, arr);
+    } else {
+        const arr = [null, null, null, null];
+        arr[swap[facing]] = key;
+        map.set(wrappingKey, arr);
+    }
+    console.log("after: ", wrappingKey, " : ", map.get(wrappingKey));
+    console.log("neighbours after: ", neighbours);
+    return neighbours;
+}
+
+let start: string;
+const wall = new Set();
+
+for (let y = 0; y < mapStrings.length; y++) {
+    for (let x = 0; x < mapStrings[y].length; x++) {
+        const key = `${x + 1},${y + 1}`;
+
+        // if current point is wall or void we can't move from there
+        if (mapStrings[y][x] === "#" || mapStrings[y][x] === " ") {
+            map.set(key, []);
+            wall.add(key);
             continue;
         }
 
-        let walkable = true;
-        if (y === 0) startX = Math.min(startX, x + 1);
+        // if start is not assigned make sure it is in top row
+        if (mapStrings[y][x] === ".") {
+            if (start === undefined) start = key;
 
-        if (items[x] === "#") walkable = false;
-        map.set(`${x + 1},${y + 1}`, new Node(x + 1, y + 1, walkable));
+            const neighbours = getNeighbours(x + 1, y + 1);
+            const newNeighbours = [null, null, null, null];
 
-        border[0] = Math.max(border[0], x + 1);
-        border[1] = Math.max(border[1], y + 1);
-        border[2] = Math.min(border[2], x + 1);
-        border[3] = Math.min(border[3], y + 1);
+            // facing 0 +x 1 +y 2 -x 3 -y
+            for (let facing = 0; facing < neighbours.length; facing++) {
+                const [nx, ny] = neighbours[facing];
+
+                if (!mapStrings[ny - 1]) {
+                    console.log("no row wrapping around Y");
+                    wrapping(facing, nx, ny, key, newNeighbours);
+                } else if (!mapStrings[ny - 1][nx - 1]) {
+                    console.log("no column wrapping around X");
+                    wrapping(facing, nx, ny, key, newNeighbours);
+                } else if (mapStrings[ny - 1][nx - 1] === " ") {
+                    console.log(
+                        neighbours[facing],
+                        "is void wrapping around X, or Y, facing:",
+                        facing
+                    );
+
+                    wrapping(facing, nx, ny, key, newNeighbours);
+                } else if (mapStrings[ny - 1][nx - 1] === ".")
+                    newNeighbours[facing] = [nx, ny];
+                else if (mapStrings[ny - 1][nx - 1] === "#")
+                    newNeighbours[facing] = [];
+            }
+            console.log(
+                "neighbours right before setting to map ",
+                newNeighbours
+            );
+            map.set(
+                key,
+                newNeighbours.map(c => {
+                    return c[0] === undefined ? "" : `${c[0]},${c[1]}`;
+                })
+            );
+        }
     }
 }
+
 const directions = lines
     .at(-1)
     .trim()
     .match(/([\d]+)|([RL])/g);
 
-const start = map.get(`${startX},1`);
-
-for (let x = border[2] - 1; x < border[0] + 1; x++) {
-    emptySpace.add(`${x},${border[3] - 1}`);
-    emptySpace.add(`${x},${border[1] + 1}`);
-}
-for (let y = border[3] - 1; y < border[1] + 1; y++) {
-    emptySpace.add(`${border[2] - 1},${y}`);
-    emptySpace.add(`${border[0] + 1},${y}`);
-}
-console.log("map: ", map);
-console.log("directions: ", directions);
-console.log("border ", border);
-console.log("start: ", start);
-console.log("empty space: ", emptySpace);
+//console.log("map: ", map);
+//console.log("directions: ", directions);
+//console.log("map: ", map);
+//console.log("start: ", start);
 
 // The final password is the sum of 1000 times the row, 4 times the column, and the facing.
 // Facing is 0 for right (>), 1 for down (v), 2 for left (<), and 3 for up (^)
 let facing = 0;
 let position = start;
-console.log(`start position: ${position.x}, ${position.y}`);
+console.log(`start position: `, position);
 
 for (const d of directions) {
     console.log("command: ", d);
@@ -86,55 +130,30 @@ for (const d of directions) {
     else {
         let steps = parseInt(d);
         while (steps) {
-            let [x, y] = position.getNeighboursKeys()[facing];
-
-            let key = `${x},${y}`;
-            let next = map.get(key);
-
-            if (next !== undefined) {
-                console.log(`there is next node: ${next.x},${next.y}`);
-                // there is node
-                if (next.walkable) position = next;
-                else break;
-            } else {
-                // there is no node
-                console.log(`there is no next node: ${x},${y}`);
-
-                console.log("empty space has key: ", emptySpace.has(key));
+            const next = map.get(position);
+            console.log("currently facing: ", facing);
+            console.log("possible next positions: ", next);
+            if (!next[facing]) break;
+            const [, x, y] = next[facing]
+                .match(/^(-?\d+),(-?\d+)/)
+                .map(n => parseInt(n));
+            if (x < 0 || y < 0) {
+                console.log(`[${x},${y}]: ${map.get(next[facing])}`);
+                console.log("next[facing]: ", next[facing]);
                 console.log(
-                    `we are going to wrap by ${facing % 2 === 0 ? "x" : "y"}`
+                    "map.get(next[facing])[facing]: ",
+                    map.get(next[facing])[facing]
                 );
-                console.log("facing: ", facing);
-
-                if (emptySpace.has(key)) {
-                    console.log("we try to wrap");
-                    const borderNeighbours = [
-                        [border[2], position.y], // 0 - right;
-                        [position.x, border[3]], // 1 - down
-                        [border[0], position.y], // 2 - left
-                        [position.x, border[1]], // 3 - up
-                    ];
-
-                    [x, y] = borderNeighbours[facing];
-                    console.log(`after wrap coordinates will be: ${x}, ${y}`);
-                    key = `${x},${y}`;
-                    next = map.get(key);
-
-                    if (next !== undefined) {
-                        console.log("there is node with this coordinates");
-                        if (next.walkable) {
-                            position = next;
-                            console.log("it is walkable");
-                        } else {
-                            console.log("it is not walkable");
-                            break;
-                        }
-                    }
-                }
+                const nextTrue = map.get(next[facing])[facing];
+                if (wall.has(nextTrue) || !nextTrue) break;
+                position = nextTrue;
+                console.log("new position after wrapping X: ", position);
+            } else {
+                console.log(`facing ${facing} move to:`, next[facing]);
+                position = next[facing];
             }
 
             steps--;
-            console.log(`new position: ${position.x}, ${position.y}`);
         }
     }
 }
