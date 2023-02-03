7642f462646dce7aab42f18702638c8354cf7fc4
octobercold <theminddepth@gmail.com>
2023-02-03 17:24:27 +0000

    aoc 2022 part one bug fixed drawing added for debugging

--- advent-of-code-2022/src/day22.ts
+++ advent-of-code-2022/src/day22.ts
@@ -1,9 +1,8 @@
-import { walkUpBindingElementsAndPatterns } from "typescript";
 import { fileReader } from "./utils/fileReader";
 const lines = fileReader(22);
-console.log("lines: ", lines);
+//console.log("lines: ", lines);
 const mapStrings = lines.slice(0, -2);
-console.log("mapStrings: ", mapStrings);
+//console.log("mapStrings: ", mapStrings);
 
 const map: Map<string, string[]> = new Map();
 function getNeighbours(x: number, y: number): number[][] {
@@ -33,8 +32,6 @@ function wrapping(
 
     const swap = { 0: 2, 1: 3, 2: 0, 3: 1 };
 
-    console.log("before: ", wrappingKey, " : ", map.get(wrappingKey));
-
     if (map.has(wrappingKey)) {
         const arr = map.get(wrappingKey);
         arr[swap[facing]] = key;
@@ -44,8 +41,7 @@ function wrapping(
         arr[swap[facing]] = key;
         map.set(wrappingKey, arr);
     }
-    console.log("after: ", wrappingKey, " : ", map.get(wrappingKey));
-    console.log("neighbours after: ", neighbours);
+
     return neighbours;
 }
 
@@ -75,28 +71,17 @@ for (let y = 0; y < mapStrings.length; y++) {
                 const [nx, ny] = neighbours[facing];
 
                 if (!mapStrings[ny - 1]) {
-                    console.log("no row wrapping around Y");
                     wrapping(facing, nx, ny, key, newNeighbours);
                 } else if (!mapStrings[ny - 1][nx - 1]) {
-                    console.log("no column wrapping around X");
                     wrapping(facing, nx, ny, key, newNeighbours);
                 } else if (mapStrings[ny - 1][nx - 1] === " ") {
-                    console.log(
-                        neighbours[facing],
-                        "is void wrapping around X, or Y, facing:",
-                        facing
-                    );
-
                     wrapping(facing, nx, ny, key, newNeighbours);
                 } else if (mapStrings[ny - 1][nx - 1] === ".")
                     newNeighbours[facing] = [nx, ny];
                 else if (mapStrings[ny - 1][nx - 1] === "#")
                     newNeighbours[facing] = [];
             }
-            console.log(
-                "neighbours right before setting to map ",
-                newNeighbours
-            );
+
             map.set(
                 key,
                 newNeighbours.map(c => {
@@ -112,7 +97,6 @@ const directions = lines
     .trim()
     .match(/([\d]+)|([RL])/g);
 
-//console.log("map: ", map);
 //console.log("directions: ", directions);
 //console.log("map: ", map);
 //console.log("start: ", start);
@@ -120,40 +104,77 @@ const directions = lines
 // The final password is the sum of 1000 times the row, 4 times the column, and the facing.
 // Facing is 0 for right (>), 1 for down (v), 2 for left (<), and 3 for up (^)
 let facing = 0;
+const result = new Map();
 let position = start;
+result.set(position, facing);
 console.log(`start position: `, position);
 
-for (const d of directions) {
+function draw(position: string, d, facing) {
+    const convert = [">", "v", "<", "^"];
+
+    function generateVicinity(position: string) {
+        const [, column, row] = position
+            .match(/^(-?\d+),(-?\d+)/)
+            .map(n => parseInt(n));
+        const trueY = row - 1;
+        const trueX = column - 1;
+        const vicinity = [];
+        for (let y = trueY - 5; y < trueY + 5; y++) {
+            const row = [];
+            for (let x = trueX - 5; x < trueX + 5; x++) {
+                if (!mapStrings[y]) continue;
+                if (!mapStrings[y][x]) continue;
+                let symbol = mapStrings[y][x];
+                if (symbol === "." && result.has(`${x + 1},${y + 1}`))
+                    symbol = convert[result.get(`${x + 1},${y + 1}`)];
+                row.push(symbol);
+            }
+            vicinity.push(row);
+        }
+        return vicinity;
+    }
+
     console.log("command: ", d);
-    facing = Math.abs(facing % 4);
-    if (d === "R") facing++;
-    else if (d === "L") facing--;
-    else {
+    console.log("currrently facing: ", convert[facing]);
+
+    generateVicinity(position).forEach(row => {
+        console.log(row.join(""));
+    });
+    console.log("\n");
+}
+
+for (const d of directions) {
+    if (d === "R") {
+        console.log("Change of direction R");
+        facing++;
+    } else if (d === "L") {
+        if (facing === 0) {
+            facing = 3;
+        } else {
+            facing--;
+        }
+        console.log("Change of direction L");
+    } else {
+        facing = Math.abs(facing % 4);
         let steps = parseInt(d);
         while (steps) {
             const next = map.get(position);
-            console.log("currently facing: ", facing);
-            console.log("possible next positions: ", next);
+
             if (!next[facing]) break;
             const [, x, y] = next[facing]
                 .match(/^(-?\d+),(-?\d+)/)
                 .map(n => parseInt(n));
             if (x < 0 || y < 0) {
-                console.log(`[${x},${y}]: ${map.get(next[facing])}`);
-                console.log("next[facing]: ", next[facing]);
-                console.log(
-                    "map.get(next[facing])[facing]: ",
-                    map.get(next[facing])[facing]
-                );
                 const nextTrue = map.get(next[facing])[facing];
                 if (wall.has(nextTrue) || !nextTrue) break;
                 position = nextTrue;
-                console.log("new position after wrapping X: ", position);
+                result.set(position, facing);
+                draw(position, d, facing);
             } else {
-                console.log(`facing ${facing} move to:`, next[facing]);
                 position = next[facing];
+                result.set(position, facing);
+                draw(position, d, facing);
             }
-
             steps--;
         }
     }
